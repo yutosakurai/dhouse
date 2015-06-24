8068a4e3ccb84dcc77c995131fd4d7c3a109d775
hyuito <0303hyuito@gmail.com>
2015-06-25 01:03:29 +0900

    dhouseサイト概形が完成しました
    dhouseのトップページは概形完成です。（あとはフッター、ナビゲーションの編集、トップ画のスラドショーを入れていい感じにして完成です）
    サブページはまだサービスの大まかな内容が伝わる程度しかできてないので、二日後の完成を目指します。

--- js/dhouse.js
+++ js/dhouse.js
@@ -14,8 +14,53 @@ $('#photoex').hide();
 $('#watchex').hide();
 $('#lightex').hide();
 $('#treeex').hide();
-$('#londonexplain').hide();
+$('#aboutexplain').hide();
+$('#flow1explain').hide();
+$('#flow2explain').hide();
+$('#featuresexplain').hide();
 
+// 「about d.house」hover時に説明フェードイン
+ 
+$('#about').hover(
+function () {
+$('#aboutexplain').stop(true, true).fadeIn(fadeinSP);
+}, function () {
+$('#aboutexplain').stop(true, true).fadeOut(fadeoutSP);
+}
+);
+
+
+// 「flow1」hover時に説明フェードイン
+ 
+$('#flow1').hover(
+function () {
+$('#flow1explain').stop(true, true).fadeIn(fadeinSP);
+}, function () {
+$('#flow1explain').stop(true, true).fadeOut(fadeoutSP);
+}
+);
+
+
+// 「flow2」hover時に説明フェードイン
+ 
+$('#flow2').hover(
+function () {
+$('#flow2explain').stop(true, true).fadeIn(fadeinSP);
+}, function () {
+$('#flow2explain').stop(true, true).fadeOut(fadeoutSP);
+}
+);
+
+
+// 「features」hover時に説明フェードイン
+ 
+$('#features').hover(
+function () {
+$('#featuresexplain').stop(true, true).fadeIn(fadeinSP);
+}, function () {
+$('#featuresexplain').stop(true, true).fadeOut(fadeoutSP);
+}
+);
  
 // 「pc」hover時にサブ画フェードイン
  
@@ -72,5 +117,26 @@ $('#treeex').stop(true, true).fadeOut(fadeoutSP);
 );
 
 
+});
+
+
+// pagetopスクロース
+
+$(document).ready(function() {
+
+var pagetop = $('.pagetopbtn');
+
+$(window).scroll(function () {
+if ($(this).scrollTop() > 100) {
+pagetop.fadeIn();
+} else {
+pagetop.fadeOut();
+}
+});
+
+pagetop.click(function () {
+$('body, html').animate({ scrollTop: 0 }, 500);
+return false;
+});
 });
 
